def2b6648acd3d6e5f4b84332dd0ca9534e34c70
Pedro Vicente <pedrovicente@poli.ufrj.br>
2021-01-25 21:00:57 -0300

    Change API URL to HTTPS

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const fetchData = async (setState) => {
   const pages = Array.from(Array(10).keys());
-  const API_URL = 'http://api.tvmaze.com/shows';
+  const API_URL = 'https://api.tvmaze.com/shows';
   const promises = [];
 
   pages.map((page) => promises.push(axios.get(`${API_URL}?page=${page}`)

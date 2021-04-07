10fd3202b0fcb62a623cac2d9928a69af3136d1e
Pedro Vicente <pedrovicente@poli.ufrj.br>
2021-04-07 16:58:32 -0300

    Add UI to fav a tv show

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { areEqual } from 'react-window';
-import { ExternalLink } from 'react-feather';
+import { ExternalLink, Star } from 'react-feather';
 
 import { getStyle } from '../utils/utils';
 
@@ -10,27 +10,39 @@ const Item = React.memo(({
   item,
   style,
   isDragging,
-}) => (
-  <div
-    {...provided.draggableProps}
-    {...provided.dragHandleProps}
-    ref={provided.innerRef}
-    style={getStyle({ provided, style, isDragging })}
-    className="
-      border-solid
-      border-blue-400
-      border-4
-      rounded"
-  >
-    <div className="flex items-center md:justify-between">
-      <img className="p-1 h-32 w-24 rounded mr-2 md:ml-4" src={item.image?.medium || 'https://via.placeholder.com/210x295'} alt={item.name} />
-      <p className="font-bold text-gray-800 text-xl md:text-3xl md:mr-8">{item.name}</p>
+}) => {
+  const [isFav, setIsFav] = useState(false);
+
+  return (
+    <div
+      {...provided.draggableProps}
+      {...provided.dragHandleProps}
+      ref={provided.innerRef}
+      style={getStyle({ provided, style, isDragging })}
+      className="
+        border-solid
+        border-blue-400
+        border-4
+        rounded"
+    >
+      <div className="flex items-center md:justify-between">
+        <img className="p-1 h-32 w-24 rounded mr-2 md:ml-4" src={item.image?.medium || 'https://via.placeholder.com/210x295'} alt={item.name} />
+        <p className="font-bold text-gray-800 text-xl md:text-3xl md:mr-8">{item.name}</p>
+      </div>
+      <a className="absolute top-2 right-1" href={item.url} target="_blank" rel="noreferrer">
+        <ExternalLink />
+      </a>
+      <button onClick={() => setIsFav(!isFav)} className="absolute top-2 right-9" type="button">
+        {isFav
+          ? (
+            <Star fill="yellow" strokeWidth={2} />
+          ) : (
+            <Star strokeWidth={2} />
+          )}
+      </button>
     </div>
-    <a className="absolute top-2 right-1" href={item.url} target="_blank" rel="noreferrer">
-      <ExternalLink />
-    </a>
-  </div>
-), areEqual);
+  );
+}, areEqual);
 
 Item.propTypes = {
   item: PropTypes.instanceOf(Object).isRequired,
